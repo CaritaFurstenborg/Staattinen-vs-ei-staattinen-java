aa7f2fd163ccdc626ccac4776145402846bc8076
CaritaFurstenborg <carita.furstenborg@gmail.com>
2018-02-07 09:15:49 +0200

    Update Staattinen.java

--- Staattinen.java
+++ Staattinen.java
@@ -5,19 +5,20 @@ public class Staattinen {
 
     
     public static void main(String[] args) {
-        // Molemmat metodit voi ajaa tästä staattisesta main metodista
-        // metodi staattinen() toimii suoraan näin:
+        // Molemmat metodit voi ajaa tästä staattisesta main metodista (luokan ainoa)
+        // metodi staattinen() toimii suoraan näin koska tätä metodia on vain yksi luokassa jota on vain yksi:
         staattinen();
         
-        // Staattinen tarkoittaa (one instance of)
+        // Staattinen tarkoittaa (one instance of), staatista objektia voi olla vain yksi ohjelmassa.
         // Koska ajava ohjelma "public STATIC void main(String[] args) on staattinen voidaan ajaa vaan staattista tavaraa täällä
         
-        // Jos taas halutaan ajaa eiStaattinen() metodi joka ei siis ole staattinen joudutaan tekemään näin:
+        // Jos taas halutaan ajaa eiStaattinen() metodi joka ei siis ole staattinen, eli näitä voi olla monta ilmentymää,
+        // joudutaan tekemään näin:
         Staattinen stat = new Staattinen();
         stat.eiStaattinen();
         
-        // Ei staattinen metodi vaatii uuden objektin luokasta jossa se sijaitsee, jotta sitä voidaan kutsua
-        // tässä tapauksessa tämä pääluokka nimeltä Staattinen
+        // Ei staattinen metodi vaatii uuden objektin luokasta jossa se sijaitsee, jotta sitä voidaan kutsua. 
+        // Näitä instanceja voi olla useampi joten täytyy määrittää mille niistä metodia kutsutaan.
         
     }
     
